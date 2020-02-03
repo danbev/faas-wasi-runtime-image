8cf28d45dd054116e0b7661b54f1c6a111f97427
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-02-03 07:11:58 +0100

    src: add rustfmt changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
-use std::{fs::File, io::Read, env};
-use hyper::server::{Http, Service, Request, Response};
-use futures::future::FutureResult;
-use hyper::{Get, Post, StatusCode, Method, header::Headers, Body};
 use cranelift_codegen::settings;
 use cranelift_native;
-use wasmtime_jit::{Context as WasmContext, RuntimeValue, ActionOutcome, ActionError};
+use futures::future::FutureResult;
+use hyper::server::{Http, Request, Response, Service};
+use hyper::{header::Headers, Body, Get, Method, Post, StatusCode};
+use std::{env, fs::File, io::Read};
+use wasmtime_jit::{ActionError, ActionOutcome, Context as WasmContext, RuntimeValue};
 
-use cloudevents::v02::{CloudEvent};
+use cloudevents::v02::CloudEvent;
 
 pub trait RequestExtractor {
     fn extract_args(&self, context: Context) -> Vec<RuntimeValue>;
 }
 
 pub trait ResponseHandler {
-    fn create_response(&self,
-                       result: Result<ActionOutcome, ActionError>,
-                       module_path: &str,
-                       function_name: &str) -> Response;
+    fn create_response(
+        &self,
+        result: Result<ActionOutcome, ActionError>,
+        module_path: &str,
+        function_name: &str,
+    ) -> Response;
 }
 
 //#[derive(Debug)]
@@ -29,12 +31,20 @@ pub struct WasmExecutor {
 }
 
 impl WasmExecutor {
-    fn new(function_name: String,
-           module_path: String,
-           module_binary: Vec<u8>,
-           request_handler: Box<dyn RequestExtractor>,
-           response_handler: Box<dyn ResponseHandler>) -> WasmExecutor {
-        WasmExecutor { function_name, module_path, module_binary, request_handler, response_handler }
+    fn new(
+        function_name: String,
+        module_path: String,
+        module_binary: Vec<u8>,
+        request_handler: Box<dyn RequestExtractor>,
+        response_handler: Box<dyn ResponseHandler>,
+    ) -> WasmExecutor {
+        WasmExecutor {
+            function_name,
+            module_path,
+            module_binary,
+            request_handler,
+            response_handler,
+        }
     }
 }
 
@@ -63,14 +73,21 @@ impl Service for WasmExecutor {
                 let isa = isa_builder.finish(settings::Flags::new(flag_builder));
                 let mut wasm_context = WasmContext::with_isa(isa);
 
-                let mut instance = wasm_context.instantiate_module(None, &self.module_binary).unwrap();
+                let mut instance = wasm_context
+                    .instantiate_module(None, &self.module_binary)
+                    .unwrap();
                 let context = create_context(&req);
                 let args: Vec<RuntimeValue> = self.request_handler.extract_args(context);
                 let result = wasm_context.invoke(&mut instance, &self.function_name, &args);
-                self.response_handler.create_response(result, &self.module_path, &self.function_name)
-            },
-            _ => Response::new().with_status(StatusCode::NotFound),})
-        }
+                self.response_handler.create_response(
+                    result,
+                    &self.module_path,
+                    &self.function_name,
+                )
+            }
+            _ => Response::new().with_status(StatusCode::NotFound),
+        })
+    }
 }
 
 fn create_context<'a>(req: &'a Request) -> Context<'a> {
@@ -81,24 +98,36 @@ fn create_context<'a>(req: &'a Request) -> Context<'a> {
         path: req.path().to_string(),
         query: req.query(),
         body: req.body_ref(),
-        cloudevent: None
+        cloudevent: None,
     }
 }
 
-pub fn start(req_handler: fn() -> Box<dyn RequestExtractor>, res_handler: fn() -> Box<dyn ResponseHandler>) {
+pub fn start(
+    req_handler: fn() -> Box<dyn RequestExtractor>,
+    res_handler: fn() -> Box<dyn ResponseHandler>,
+) {
     let port = env::var("PORT").expect("PORT environment variable not set");
     let addr_port = format!("0.0.0.0:{}", port);
     let addr = addr_port.parse().unwrap();
-    let function_name = env::var("FUNCTION_NAME").expect("FUNCTION_NAME environment variable not set");
+    let function_name =
+        env::var("FUNCTION_NAME").expect("FUNCTION_NAME environment variable not set");
     let module_path = env::var("MODULE_PATH").expect("MODULE_PATH environment variable not set");
-    println!("WASI Runtime started. Port: {}, Module path: {}", port, module_path);
+    println!(
+        "WASI Runtime started. Port: {}, Module path: {}",
+        port, module_path
+    );
     let binary: Vec<u8> = read_module(&module_path);
-    let server = Http::new().bind(&addr, 
-                                  move || Ok(WasmExecutor::new(function_name.clone(),
-                                                               module_path.clone(),
-                                                               binary.clone(),
-                                                               req_handler(),
-                                                               res_handler()))).unwrap();
+    let server = Http::new()
+        .bind(&addr, move || {
+            Ok(WasmExecutor::new(
+                function_name.clone(),
+                module_path.clone(),
+                binary.clone(),
+                req_handler(),
+                res_handler(),
+            ))
+        })
+        .unwrap();
     server.run().unwrap();
 }
 
